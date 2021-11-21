5e9610d1a854eeb5e803f13d1dd3428b53d11fae
burcuozbek <kburcuozbek@gmail.com>
2021-11-21 15:42:33 +0300

    delete films from storage

--- project.js
+++ project.js
@@ -47,5 +47,10 @@ function addFilm(e) {
 function deleteFilm(e) {
   if (e.target.id === "delete-film") {
     ui.deleteFilmFromUI(e.target);
+    storage.deleteFilmFromStorage(
+      e.target.parentElement.previousElementSibling.previousElementSibling
+        .textContent
+    );
+    ui.displayMessages("Silme işlemi başarılı!", "success");
   }
 }
