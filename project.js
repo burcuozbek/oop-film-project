43b860399d2d7b76279604bda3ab5c053826a082
burcuozbek <kburcuozbek@gmail.com>
2021-11-21 14:11:34 +0300

    js interface upd

---
+++ project.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("film-form");
+const titleElement = document.getElementById("title");
+const directorElement = document.getElementById("director");
+const urlElement = document.querySelector("#url");
+
+//UI Objesini Başlatma
+const ui = new UI();
+
+//Tüm eventleri yükleme
+
+eventListeners();
+
+function eventListeners() {
+  form.addEventListener("submit", addFilm);
+}
+function addFilm(e) {
+  const title = titleElement.value;
+  const director = directorElement.value;
+  const url = urlElement.value;
+
+  if (title === "" || director === "" || url === "") {
+    //Hata
+  } else {
+    //Yeni Film
+    const newFilm = new Film(title, director, url);
+    ui.addFilmToUI(newFilm); //Arayüze film ekleme
+  }
+
+  ui.clearInputs(titleElement, urlElement, directorElement);
+  e.preventDefault();
+}
